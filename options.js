427d9de82ea5b83e228067165aa424eca328fdb9
SUGAWARA Hiroshi <sncts35326@gmail.com>
2015-11-21 04:37:16 +0900

    ちゃんと動く用になってから ver.1 として登録しようと思っていたが、思ったよりも大きくなってきたので途中の状態だけどコミットしておく

---
+++ options.js
@@ -0,0 +1,25 @@
+"use strict";
+
+document.getElementById('save').onclick = function () {
+  chrome.storage.sync.set({
+    date_first: document.querySelector('[name="date_first"]').value,
+    date_last: document.querySelector('[name="date_last"]').value,
+    block_time_first: document.querySelector('[name="block_time_first"]').value,
+    block_time_last: document.querySelector('[name="block_time_last"]').value
+  }, function () {
+  });
+};
+
+window.onload = function () {
+  chrome.storage.sync.get([
+    "date_first",
+    "date_last",
+    "block_time_first",
+    "block_time_last"
+  ], function (data) {
+    document.querySelector('[name="date_first"]').setAttribute("value", data.date_first);
+    document.querySelector('[name="date_last"]').setAttribute("value", data.date_last);
+    document.querySelector('[name="block_time_first"]').setAttribute("value", data.block_time_first);
+    document.querySelector('[name="block_time_last"]').setAttribute("value", data.block_time_last);
+  });
+};
