2336c716ca7960726d0fbc72def705ba02def65e
Hiroshi Sugawara <5000164@users.noreply.github.com>
2016-05-15 07:32:25 +0900

    オプションの見た目を整えて、初期値を設定して、クリア機能を追加

--- options.js
+++ options.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// 設定内容を保存する
 document.getElementById('save').onclick = function () {
   chrome.storage.local.set({
     date_first: document.querySelector('[name="date_first"]').value,
@@ -9,13 +10,22 @@ document.getElementById('save').onclick = function () {
   });
 };
 
+// 保存内容をすべてクリアする
+document.getElementById('clear').onclick = function () {
+  chrome.storage.local.clear(function () {
+    location.reload();
+  });
+};
+
+// 設定されている内容を画面に表示する
 window.onload = function () {
-  chrome.storage.local.get([
-    "date_first",
-    "date_last",
-    "block_time_first",
-    "block_time_last"
-  ], function (data) {
+  var defaults = {
+    date_first: "2016/01/01",
+    date_last: "2016/12/31",
+    block_time_first: "14:00",
+    block_time_last: "15:30"
+  };
+  chrome.storage.local.get(defaults, function (data) {
     document.querySelector('[name="date_first"]').setAttribute("value", data.date_first);
     document.querySelector('[name="date_last"]').setAttribute("value", data.date_last);
     document.querySelector('[name="block_time_first"]').setAttribute("value", data.block_time_first);
